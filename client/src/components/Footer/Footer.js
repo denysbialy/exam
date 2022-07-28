259794749e985ac326cbf36d83d528d35f201f5b
denysbialy <dnsbls11@gmail.com>
2022-07-28 23:46:38 +0300

    Refactored Component Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
 import styles from './Footer.module.sass';
 import CONSTANTS from '../../constants';
 
-class Footer extends Component {
-    topFooterItemsRender = (item) => (
-      <div key={item.title}>
-        <h4>{item.title}</h4>
-        {item.items.map((i) => <a key={i} href="https://google.com">{i}</a>)}
-      </div>
-    );
+function Footer(){
+  const topFooterItemsRender = item => (
+    <div key={item.title}>
+      <h4>{item.title}</h4>
+      <ul>{item.items.map(i => (<li key={i}><a href='https://google.com'>{i}</a></li>))}</ul>
+    </div>
+  );
+  const topFooterRender = () => CONSTANTS.FooterItems.map(item => topFooterItemsRender(item));
 
-    topFooterRender() {
-      return CONSTANTS.FooterItems.map((item) => this.topFooterItemsRender(item));
-    }
-
-    render() {
-      return (
-        <div className={styles.footerContainer}>
-          <div className={styles.footerTop}>
-            <div>
-              {this.topFooterRender()}
-            </div>
-          </div>
+    return (
+      <footer className={styles.footerContainer}>
+        <div className={styles.footerTop}>
+          <div>{topFooterRender()}</div>
         </div>
-      );
-    }
+      </footer>
+    );
 }
 
 export default Footer;
