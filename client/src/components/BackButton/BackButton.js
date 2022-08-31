e70311b63626429e5caecae66c88955636edc603
denysbialy <dnsbls11@gmail.com>
2022-08-31 23:35:05 +0300

    Refactoring component BackButton

--- client/src/components/BackButton/BackButton.js
+++ client/src/components/BackButton/BackButton.js
@@ -1,17 +1,12 @@
-import React from 'react';
 import { useHistory, withRouter } from 'react-router-dom';
 import styles from './BackButton.module.sass';
 
-const BackButton = props => {
+const BackButton = () => {
   const history = useHistory();
-  function clickHandler () {
-    history.goBack();
-  }
-
   return (
-    <div onClick={clickHandler} className={styles.buttonContainer}>
-      <span>Back</span>
-    </div>
+    <button onClick={() => history.goBack()} className={styles.buttonContainer}>
+      Back
+    </button>
   );
 };
 
